0de049c7dedb6c3899f2e5cc0bb70d07a9a79ec3
hyyu <hubert080116@gmail.com>
2019-09-10 22:36:01 +0800

    ch12

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -133,8 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
